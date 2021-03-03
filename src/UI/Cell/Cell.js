5896f232589ed101650f561a0542b44d663af35b
OLG <lastdayoflife2@gmail.com>
2021-03-04 01:25:38 +0200

    feat: add flexible design

--- src/UI/Cell/Cell.js
+++ src/UI/Cell/Cell.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import useWindowSize from '../../hooks/useWindowSize';
 
 const Cell = ({ id, x, y, value }) => {
+	const size = useWindowSize();
 	const position = () => {
-		let width = 127,
-			height = 124;
-		return {
-			transform: `translate(${y * width}px, ${x * height}px)`,
-			height: '107px',
-			width: '107px'
-		};
+		if (size.width > 700) {
+			let width = 127,
+				height = 124;
+			return {
+				transform: `translate(${y * width}px, ${x * height}px)`,
+				height: '107px',
+				width: '107px'
+			};
+		} else {
+			let width = 90,
+				height = 90;
+			return {
+				transform: `translate(${y * width + 5}px, ${x * height}px)`,
+				height: '80px',
+				width: '80px'
+			};
+		}
 	};
 
 	return (
--- src/UI/CellEmpty/CellEmpty.js
+++ src/UI/CellEmpty/CellEmpty.js
@@ -1,14 +1,27 @@
 import React from 'react';
+import useWindowSize from '../../hooks/useWindowSize';
 
 const CellEmpty = (id, x, y, value) => {
+	const size = useWindowSize();
 	const position = () => {
-		let width = 123,
-			height = 123;
-		return {
-			transform: `translate(${y * width}px, ${x * height}px)`,
-			height: '107px',
-			width: '107px'
-		};
+		if (size.width > 700) {
+			let width = 123,
+				height = 123;
+			return {
+				transform: `translate(${y * width}px, ${x * height}px)`,
+				height: '107px',
+				width: '107px'
+			};
+		} else {
+			let width = 90,
+				height = 90;
+			return {
+				transform: `translate(${y * width + 5}px, ${x * height}px)`,
+				height: '80px',
+				width: '80px',
+				marginBottom: '9px'
+			};
+		}
 	};
 
 	return <div style={position()} className='play-tiles-bg' />;
